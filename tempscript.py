2b99ae043a3526c339b3ea6f67cd8c19f985d566
cstein6 <119823436+cstein6@users.noreply.github.com>
2024-02-08 17:01:36 -0700

    Update tempscript.py
    Removed unnecessary module, cleaned up syntax, added instructions for use by people who aren't on myself on my own PC.. and added replacable portions of file paths to assist in setting up the script for personal use by, again, other people.

--- tempscript.py
+++ tempscript.py
@@ -1,39 +1,41 @@
 import shutil
 import os
 import subprocess
-import time
 
-#clears out temp files in selected folders, runs an advanced options disk clean, and flushes dns resolver cache with 0 interaction.. except;
-
+# Clears out temp files in specified folders and flushes dns resolver cache with 0 user interaction.
 
 def cleartemp(temp):
     for folder in temp:
         try:
             contents = os.listdir(folder)
             for item in contents:
-                filepath = os.path.join(folder, item)
+                path = os.path.join(folder, item)
                 try:
-                    if os.path.isfile(filepath):
-                        os.unlink(filepath)
-                    elif os.path.isdir(filepath):
-                        shutil.rmtree(filepath)
+                    if os.path.isfile(path):
+                        os.unlink(path)
+                    elif os.path.isdir(path):
+                        shutil.rmtree(path)
                 except Exception:
                     pass
         except Exception:
             pass
 
-# folders to clear
+# specifies folders to clear, replace "PATH_TO_COD_FOLDER" and "<YourUserName>" with... your cod folder path and ur windows username.
 temp = [
-    r"C:\Users\Administrator\AppData\Local\Temp",
+    r"C:\Users\<YourUserName>\AppData\Local\Temp",
     r"C:\Windows\Temp",
     r"C:\Windows\Prefetch",
-    r"C:\CoD\Call of Duty\_retail_\xpak_cache",
-    r"C:\CoD\Call of Duty\_retail_\telescopeCache",
+    r"C:\PATH_TO_COD_FOLDER\_retail_\xpak_cache",
+    r"C:\PATH_TO_COD_FOLDER\_retail_\telescopeCache",
 #optional
-    #r"C:\CoD\Call of Duty\_retail_\telescopeStorage"
-    #r"C:\CoD\Call of Duty\_retail_\cod23\shadercache" #shaders(rarely necessary)
+    #r"C:\CoD\Call of Duty\_retail_\telescopeStorage" # makes you agree to terms of use again, idrk what this is or if its even helpful/necessary to clean...
+    #r"C:\CoD\Call of Duty\_retail_\cod23\shadercache" # shaders.
+        # I only recommend clearing shader cache if you either; 1. Updated GPU drivers and want a fresh install of shaders. 
+        #2. Changed texture filtering options in a graphics control panel, and are experiencing sharpening issues or repeatedly getting "shaders installing" on consecutive game launches. or 
+        #3. Are experiencing texture issues that can't be resolved through in-game or config file tweaks, and just want to try it.
+    #TIP: no matter what, do not interrupt the fresh shader install when you launch the game after clearing cache or updating GPU driver.
 ]
 
-cleartemp(temp)
+cleartemp(temp) # clears folders
 
-subprocess.run(["ipconfig", "/flushdns"])
+subprocess.run(["ipconfig", "/flushdns"]) # clears DNS cache
