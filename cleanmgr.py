f5263c4984e7e787659b54515c1e5a6276b12437
cstein6 <119823436+cstein6@users.noreply.github.com>
2024-02-08 17:03:33 -0700

    Update cleanmgr.py
    readability.

--- cleanmgr.py
+++ cleanmgr.py
@@ -1,7 +1,5 @@
+# For first time users, open cmd prompt and run "cleanmgr /sageset:99". A disk cleanup settings dialogue will open and you can select every category to routinely clean.
+# After clicking OK, the dialogue will close, and you can run this script to use those options. To change them, or to use on a new PC/install, repeat the process.
 import subprocess
 
-#For first time users, open cmd prompt and run "cleanmgr /sageset:99"
-#a dialogue will open and you can select every category to routinely clean.
-#after clicking OK, the dialogue will close, and you can run this script to use those options.
-#to change the set options, or to use on a new PC/install, repeat this process.
 subprocess.run(["cleanmgr", "/sagerun:99"])
